4ab9a74613ec06d1991b067ffdd75f98b2682f5e
Saahil Rachh <saahilrachh1234@gmail.com>
2024-02-21 20:44:34 -0700

    feat: new stack, routing, sidebar

---
+++ src/Community.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Community() {
+	return (
+		<div>
+			<h1 style={{ color: "black", fontFamily: "Arial, sans-serif" }}>
+				Community page
+			</h1>
+		</div>
+	);
+}
+
+export default Community;
