d83202445f18f31efa6a40b05c4fcda81192179b
jd-lo <jdlo@ualberta.ca>
2024-03-05 21:50:48 -0700

    Actually add Mantine

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, createContext} from "react";
 import "./App.css";
 import "./output.css";
+import '@mantine/core/styles.css';
 import Navigation from "./components/Navbar.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Bookings from "./Bookings.jsx";
@@ -11,6 +12,7 @@ import Signout from "./Signout.jsx";
 import Community from "./Community.jsx";
 import Feedback from "./Feedback.jsx";
 import Modal from 'react-modal';
+import { MantineProvider } from '@mantine/core';
 
 export const HostContext = createContext();
 export const UserContext = createContext();
@@ -23,20 +25,22 @@ function App() {
 	return (
 		<HostContext.Provider value = {{ host }}>
 			<UserContext.Provider value = {{user, setUser}}>
-				<Router>
-					<div className="flex h-screen w-screen flex-col md:flex-row md:overflow-hidden body-white py-0 px-[30px] gap-[40px]">
-						<Navigation />
-						<Routes>
-							<Route path="/bookings" element={<Bookings />} />
-							<Route path="/events" element={<Events />} />
-							<Route path="/community" element={<Community />} />
-							<Route path="/statistics" element={<Statistics />} />
-							<Route path="/profile" element={<Profile />} />
-							<Route path="/feedback" element={<Feedback />} />
-							<Route path="/signout" element={<Signout />} />
-						</Routes>
-					</div>
-				</Router>
+				<MantineProvider>
+					<Router>
+						<div className="flex h-screen w-screen flex-col md:flex-row md:overflow-hidden body-white py-0 px-[30px] gap-[40px]">
+							<Navigation />
+							<Routes>
+								<Route path="/bookings" element={<Bookings />} />
+								<Route path="/events" element={<Events />} />
+								<Route path="/community" element={<Community />} />
+								<Route path="/statistics" element={<Statistics />} />
+								<Route path="/profile" element={<Profile />} />
+								<Route path="/feedback" element={<Feedback />} />
+								<Route path="/signout" element={<Signout />} />
+							</Routes>
+						</div>
+					</Router>
+				</MantineProvider>
 			</UserContext.Provider>
 		</HostContext.Provider>
 	);
--- frontend/src/Bookings.jsx
+++ frontend/src/Bookings.jsx
@@ -1,11 +1,10 @@
 import React from "react";
+import { Chip } from "@mantine/core";
 
 function Bookings() {
 	return (
 		<div>
-			<h1 style={{ color: "black", fontFamily: "Arial, sans-serif" }}>
-				Bookings page
-			</h1>
+			<Chip defaultChecked>Testing Install of Mantine</Chip>
 		</div>
 	);
 }
