372e163077438d79480d06fcb83eb0401e968d36
KayKay321 <87404888+KayKay321@users.noreply.github.com>
2024-03-09 16:28:09 -0700

    api tests for user workingg

---
+++ backend/Booking/tests/test_api.py
@@ -0,0 +1,6 @@
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+from Booking.models import Booking
+from Booking.serializers import BookingSerializer
+
