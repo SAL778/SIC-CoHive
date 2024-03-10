6bab78228243d29aff8eb10ff2ec870adc7e5253
KayKay321 <87404888+KayKay321@users.noreply.github.com>
2024-03-09 22:59:55 -0700

    bookings test cases working

--- backend/Booking/tests/test_api.py
+++ backend/Booking/tests/test_api.py
@@ -3,6 +3,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 from Booking.models import Booking
 from Booking.serializers import BookingSerializer
+import warnings
 
 from User.models import CustomUser
 
@@ -26,13 +27,13 @@ class BookingTests(APITestCase):
         self.user.education.save()
         self.token = Token.objects.create(user=self.user)
 
-        self.ressource1 = Resources.objects.create(
+        self.resource1 = Resources.objects.create(
             name="Test Room",
             description="Test Description1",
             room_number="Test Room1",
             type="room"
         )
-        self.ressource1.save()
+        self.resource1.save()
         self.resources2 = Resources.objects.create(
             name="Test Equipment",
             description="Test Description2",
@@ -40,19 +41,175 @@ class BookingTests(APITestCase):
             type="equipment"
         )
         self.resources2.save()
+        
 
 
     def test_create_booking(self):
         url = reverse('booking-list', kwargs={'user_id': self.user.id})  # Include user_id in URL
-        data = {
-            "start_time": "2021-12-01T12:00:00Z",
-            "end_time": "2021-12-01T13:00:00Z",
-            "resources": 1,
+        data1 = {
+            "start_time": "2021-12-02T12:15",
+            "end_time": "2021-12-02T13:00",
+            "resources": "Test Equipment",
+            "visibility": True,
+            "title": "Test Booking"
+        }
+        
+        data2 = {
+            "start_time": "2021-12-02T12:15",
+            "end_time": "2021-12-02T12:30",
+            "resources": "Test Equipment",
             "visibility": True,
             "title": "Test Booking"
         }
 
-        response = self.client.post(url, data, format='json', HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(url, data1, format='json', HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+       
         self.assertEqual(response.status_code, 201)
         self.assertEqual(Booking.objects.count(), 1)
         self.assertEqual(Booking.objects.get().title, 'Test Booking')
+        self.assertEqual(Booking.objects.get().resources, self.resources2)
+        self.assertEqual(Booking.objects.get().user, self.user)
+        self.assertEqual(Booking.objects.get().visibility, True)
+        self.assertEqual(response.data['start_time'], "2021-12-02T12:15")
+        self.assertEqual(response.data['end_time'], "2021-12-02T13:00")
+        
+        response = self.client.post(url, data2, format='json', HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(Booking.objects.count(), 1)
+        
+    
+    def test_create_booking_invalid(self):
+        url = reverse('booking-list', kwargs={'user_id': self.user.id})
+        data = {
+            "start_time": "2021-12-01T12:20",
+            "end_time": "2021-12-01T13:40",
+            "resources": "Test Equipment",
+            "visibility": True,
+            "title": "Test Booking"
+        }
+        data2 = {
+            "start_time": "2021-12-01T13:00",
+            "end_time": "2021-12-01T12:30",
+            "resources": "Test Room",
+            "visibility": True,
+            "title": "Test Booking"
+        }
+        
+        response = self.client.post(url, data, format='json', HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(Booking.objects.count(), 0)
+        response = self.client.post(url, data2, format='json', HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(Booking.objects.count(), 0)
+        
+    
+    def test_get_booking(self):
+        booking = Booking.objects.create(
+            start_time="2021-12-01T12:15",
+            end_time="2021-12-01T13:00",
+            resources=self.resources2,
+            user=self.user,
+            visibility=True,
+            title="Test Booking"
+        )
+        booking.save()
+        url = reverse('booking-detail', kwargs={'pk': booking.id})
+        response=self.client.get(url, HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['title'], 'Test Booking')
+        self.assertEqual(response.data['resources'], self.resources2.id)
+        self.assertEqual(response.data['user']['id'], self.user.id)
+        self.assertEqual(response.data['visibility'], True)
+        self.assertEqual(response.data['start_time'], "2021-12-01T12:15")
+        self.assertEqual(response.data['end_time'], "2021-12-01T13:00")
+        
+        # print(response.data)    
+    
+    
+    def test_edit_booking(self):
+        
+        booking = Booking.objects.create(
+            start_time="2021-12-01T12:15",
+            end_time="2021-12-01T13:00",
+            resources=self.resources2,
+            user=self.user,
+            visibility=True,
+            title="Test Booking"
+        )
+        booking.save()
+        url = reverse('booking-detail', kwargs={'pk': booking.id})
+        data = {
+            "start_time": "2021-12-01T12:30",
+            "end_time": "2021-12-01T13:00",
+            "resources": 2,
+            "visibility": False,
+            "title": "Test Booking"
+        }
+        response=self.client.patch(url, data, format='json', HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['title'], 'Test Booking')
+        self.assertEqual(response.data['resources'], self.resources2.id)
+        self.assertEqual(response.data['user'], {})
+        self.assertEqual(response.data['visibility'], False)
+        self.assertEqual(response.data['start_time'], "2021-12-01T12:30")
+        self.assertEqual(response.data['end_time'], "2021-12-01T13:00")
+        
+    
+    def test_delete_booking(self):
+        booking = Booking.objects.create(
+            start_time="2021-12-01T12:15",
+            end_time="2021-12-01T13:00",
+            resources=self.resources2,
+            user=self.user,
+            visibility=True,
+            title="Test Booking"
+        )
+        booking.save()
+        url = reverse('booking-detail', kwargs={'pk': booking.id})
+        response=self.client.delete(url, HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Booking.objects.count(), 0)
+        
+    def test_columns(self):
+        url = reverse('column-list')
+        response=self.client.get(url, HTTP_AUTHORIZATION='Token ' + self.token.key)
+        print(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data[0]['id'], 1)
+        self.assertEqual(response.data[0]['name'], 'Test Room')
+        self.assertEqual(response.data[0]['description'], 'Test Description1')
+        self.assertEqual(response.data[0]['type'], "room")
+        self.assertEqual(response.data[1]['id'], 2)
+        self.assertEqual(response.data[1]['name'], 'Test Equipment')
+        self.assertEqual(response.data[1]['description'], 'Test Description2')
+        self.assertEqual(response.data[1]['type'], "equipment")
+        
+    def test_resources(self):
+        url=reverse('resource-list')
+        response=self.client.get(url, HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data[0],"Test Equipment")
+        self.assertEqual(response.data[1],"Test Room")
+       
+        from urllib.parse import urlencode
+
+        base_url = reverse('resource-list')
+        query_string = urlencode({'type': 'equipment'})
+        url = f"{base_url}?{query_string}"
+        response=self.client.get(url, HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data[0], "Test Equipment")
+        self.assertEqual(len(response.data), 1)
+        
+        base_url = reverse('resource-list')
+        query_string = urlencode({'type': 'room'})
+        url = f"{base_url}?{query_string}"
+        response=self.client.get(url, HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data[0], "Test Room")
+        self.assertEqual(len(response.data), 1) 
