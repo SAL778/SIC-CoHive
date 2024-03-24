9bc750872688f79525fe3946fbfd84f8fbc60101
jd-lo <jdlo@ualberta.ca>
2024-03-23 22:16:09 -0600

    Connected google sheets

--- backend/Google_drive_integration/urls.py
+++ backend/Google_drive_integration/urls.py
@@ -4,5 +4,6 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    path('files', views.fetch_drive_files, name='google-drive-get-files')
+    path('files', views.fetch_drive_files, name='google-drive-get-files'),
+    path('events', views.fetch_spreadsheet_events, name='google-get-events'),
 ]
