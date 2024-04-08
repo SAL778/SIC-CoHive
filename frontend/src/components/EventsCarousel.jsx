8593850e462fb8428f0c75ad96e53fddbaf0b530
Saahil Rachh <saahilrachh1234@gmail.com>
2024-04-08 09:55:55 -0600

    feat: events from SIC account | fix: carousel from cal

--- frontend/src/components/EventsCarousel.jsx
+++ frontend/src/components/EventsCarousel.jsx
@@ -31,7 +31,8 @@ function EventCard({ event, onClick }) {
 
 	const formatDate = (dateString) => {
 		// Parse the date string
-		const [monthStr, day, year] = dateString.split("/");
+		//"2024-05-02T13:00:00-06:00"
+		const [year, monthStr, day] = dateString.split("T")[0].split("-");
 		const month = parseInt(monthStr, 10);
 
 		// Define an array of month abbreviations
@@ -66,7 +67,7 @@ function EventCard({ event, onClick }) {
 				src={properImageSource(event.imgSrc) || fallback}
 				// Placeholder image for testing and broke image issue
 				// src = "https://png.pngtree.com/png-vector/20191126/ourmid/pngtree-image-of-cute-radish-vector-or-color-illustration-png-image_2040180.jpg"
-				alt={event.title}
+				alt={event.summary}
 				className="absolute inset-0 w-full h-full transition object-cover duration-500 ease-in-out group-hover:scale-110 z-0"
 				referrerPolicy="no-referrer"
 			/>
@@ -74,21 +75,23 @@ function EventCard({ event, onClick }) {
 			<div className="cardContent relative flex flex-col h-full justify-between z-10 text-white">
 				<h2 className="title flex items-start text-lg font-medium">
 					<i
-						style={{ backgroundColor: genHexColor(event.title) }}
+						style={{ backgroundColor: genHexColor(event.summary) }}
 						className="w-5 min-w-5 h-5 rounded-full inline-block mt-1 mr-3"
 					/>
-					{event.title}
+					{event.summary}
 				</h2>
 
 				<div className="locationTimeSection flex flex-col gap-2">
 					<span className="eventLocation block">
 						<i className="fa fa-location-dot text-white mr-3 text-lg w-[20px]" />
-						<p className="inline">{event.location}</p>
+						<p className="inline">
+							{event.location.startsWith("https") ? "REMOTE" : event.location}
+						</p>
 					</span>
 					<span className="eventTime block">
 						<i className="fa fa-clock text-white mr-3 text-lg w-[20px]" />
 						{/* <p className= "inline">{event.startTime} - {event.endTime}</p> */}
-						<p className="inline">{formatDate(event.date)}</p>
+						<p className="inline">{formatDate(event.start.dateTime)}</p>
 					</span>
 					<i className="fa fa-arrow-right ml-auto mt-4" />
 				</div>
